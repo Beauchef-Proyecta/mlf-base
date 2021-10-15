fd44618f53f72d8f89be11722da99a1cfbf12eab
Francisco Casado <fcasado@ug.uchile.cl>
2021-10-15 00:34:28 -0300

    agrega documentacion

--- 05_computer_vision/color_detector.py
+++ 05_computer_vision/color_detector.py
@@ -1,32 +1,77 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
+
 
 class ColorDetector:
 
     def __init__(self, img):
         self.img = img
+
+        # Inicializamos en cero todas las imagenes que guardaremos
         self.mask = np.zeros(img.shape)
-        self.masked_img = np.zeros(img.shape)
-        self.eroded_img = np.zeros(img.shape)
-        self.dilated_img = np.zeros(img.shape)
-        self.grayscale_img = np.zeros(img.shape)
-        self.thresh_img = np.zeros(img.shape)
-        self.contoured_img = np.zeros(img.shape)
+        self.img_masked = np.zeros(img.shape)
+        self.img_eroded = np.zeros(img.shape)
+        self.img_dilated = np.zeros(img.shape)
+        self.img_gray = np.zeros(img.shape)
+        self.img_threshold = np.zeros(img.shape)
+        self.img_contoured = np.zeros(img.shape)
+
+    def process_image(self, lower_color, upper_color, kernel, threshold):
+        self.mask_image(lower_color, upper_color)
+        self.filter_image(kernel)
+        self.draw_contours(threshold)
 
     def mask_image(self, lower_color, upper_color):
         img = np.copy(self.img)
         self.mask = cv2.inRange(img, lower_color, upper_color)
-        self.masked_img = cv2.bitwise_and(img, img, mask=self.mask)
+        self.img_masked = cv2.bitwise_and(img, img, mask=self.mask)
 
     def filter_image(self, kernel):
-        img = self.masked_img
-        self.eroded_img = cv2.erode(img, kernel)
-        self.dilated_img = cv2.dilate(img, kernel)
+        # https://docs.opencv.org/trunk/d9/d61/tutorial_py_morphological_ops.html
+        img = self.img_masked
+        self.img_eroded = cv2.erode(img, kernel)
+        self.img_dilated = cv2.dilate(img, kernel)
 
-    def find_contours(self):
-        self.grayscale_img = cv2.cvtColor(self.eroded_img, cv2.COLOR_RGB2GRAY)
-        ret, self.thresh_img = cv2.threshold(self.grayscale_img, 120, 255, cv2.THRESH_BINARY)
-        contours, hierarchy = cv2.findContours(self.thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        self.contoured_img = self.img.copy()
-        cv2.drawContours(self.contoured_img, contours, -1, color=(0, 255,0), thickness=4)
+    def draw_contours(self, threshold):
+        """
+        Para hacer esta función se deben seguir los siguientes pasos:
+        1. convertir una imagen filtrada a escala de grises
+        2. aplicar un umbral (threshhold). Se sugiere truncado: type=cv2.THRESH_TRUNC
+        3. usar la funcion cv2.findContours:
+            contours, hierarchy = cv2.finContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        4. copiar la imagen original
+        5. usar cv2.drawContours(img, contours, -1, color=(0, 255,0), thickness=4)
+
+        Para mas info, consultar
+        https://learnopencv.com/contour-detection-using-opencv-python-c/
+        """
+
+        self.img_gray = cv2.cvtColor(self.img_eroded, cv2.COLOR_RGB2GRAY)
+
+        ret, self.img_threshold = cv2.threshold(self.img_eroded, threshold, 255, cv2.THRESH_TRUNC)
+
+        contours, hierarchy = cv2.findContours(self.img_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        self.img_contoured = self.img.copy()
+        cv2.drawContours(self.img_contoured, contours, -1, color=(0, 255, 0), thickness=4)
+
+    def draw_bounding_boxes(self):
+        """
+        [Propuesto]
+        1. Con el resultado de los contornos del metodo anterior,
+        encontrar aquellos contornos cuya area encerrada sea superior a un
+        numero arbitrario (por ej: 900 pixeles).
+
+        2. Luego, iterar sobre cada grupo de contornos y usar las
+        funciones cv2.boundingRect y cv2.rectangle:
+
+         Mas o menos así
+            for contour in contours:
+                  x, y, w, h = cv2.boundingRect(contour)
+                  cv2.rectangle(img, (x1, y1), (x2, y2), COLOR, line_width)
+
+        Ojo que cv2.boundingRect entrega un origen (x, y) y el alto y ancho
+        del rectangulo, pero cv2.rectangle toma dos esquinas opuestas como
+        argumentos.
+
+        """
--- 05_computer_vision/tarea_05.py
+++ 05_computer_vision/tarea_05.py
@@ -1,10 +1,8 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-from matplotlib.widgets import RangeSlider
-from mpl_toolkits.mplot3d import Axes3D
 from color_detector import ColorDetector
-from plotter import Plotter
+
 
 def main():
     img_path = 'images/arcoiris.jpg'
@@ -20,59 +18,33 @@ def main():
     # Calculamos la mascara y la aplicamos
     lower_color = np.array([100, 0, 0])
     upper_color = np.array([200, 255, 255])
+
     c.mask_image(lower_color, upper_color)
     ax[0, 1].imshow(c.mask, cmap='gray')
-    ax[0, 2].imshow(c.masked_img)
+    ax[0, 2].imshow(c.img_masked)
 
     # Se aplican operaciones morfológicas para reducir el ruido
-    kernel = np.ones((10, 10), np.uint8)
-    c.filter_image(kernel)
-    ax[1, 0].imshow(c.eroded_img)
-    ax[1, 1].imshow(c.eroded_img)
+    kernel = np.ones((20, 20), np.uint8)
+
+    c.filter_image(kernel=kernel)
+    ax[1, 0].imshow(c.img_eroded)
+    ax[1, 1].imshow(c.img_dilated)
 
     # Busqueda de contornos
-    c.find_contours()
-    ax[2, 0].imshow(c.grayscale_img, cmap='gray')
-    ax[2, 1].imshow(c.thresh_img, cmap='gray')
+    c.draw_contours(threshold=180)
+    ax[2, 0].imshow(c.img_gray, cmap='gray')
+    ax[2, 1].imshow(c.img_threshold, cmap='gray')
+    ax[2, 2].imshow(c.img_contoured)
 
-    ax[2, 2].imshow(c.contoured_img)
-
-
-
-
-
-
-    # Aplicar operaciones morfológicas para eliminar ruido
-    # Esto corresponde a hacer un Opening
-    # https://docs.opencv.org/trunk/d9/d61/tutorial_py_morphological_ops.html
-    # Operacion morfologica erode
-
-    # Operacion morfologica dilate
-
-    # Busca contornos de blobs
-    # https://docs.opencv.org/trunk/d3/d05/tutorial_py_table_of_contents_contours.html
-    """
-    # Iterar sobre contornos y dibujar bounding box de los patos
-    for cnt in contours:
-        # Obtener rectangulo que bordea un contorno
-
-        # Filtrar por area minima
-        if AREA > min_area:  # DEFINIR AREA
-    # Dibujar rectangulo en el frame original
-
-    # Se muestra en una ventana llamada "patos" la observación del simulador
-    # con los bounding boxes dibujados
-    cv2.imshow('patos', cv2.cvtColor(obs, cv2.COLOR_RGB2BGR))
-    # Se muestra en una ventana llamada "filtrado" la imagen filtrada
-    cv2.imshow('filtrado', image)
-
-    """
+    """ OBS: Fsta función permite poder cerrar la venta apretando la tecla q """
     def close(event):
         if event.key == 'q':
             plt.close(event.canvas.figure)
 
-    cid = plt.gcf().canvas.mpl_connect("key_press_event", close)
+    plt.gcf().canvas.mpl_connect("key_press_event", close)
+    """ FIN OBS """
 
+    # Esta linea es muy importante: es la que "abre" la ventana de los graficos
     plt.show()
 
 
